2588e81ec2a8f68462aadd4b95605dfcf3a9a4ba
Gewinum <gewinum@gmail.com>
2024-08-24 22:47:09 +0400

    feat: add rate limit to rest api

--- restapi-server/internal/dependency/dependency.go
+++ restapi-server/internal/dependency/dependency.go
@@ -4,7 +4,10 @@ import (
 	"github.com/Gewinum/go-weather-microservices/restapi-server/internal/client"
 	"github.com/Gewinum/go-weather-microservices/restapi-server/internal/config"
 	"github.com/Gewinum/go-weather-microservices/restapi-server/internal/handlers"
+	ratelimit "github.com/JGLTechnologies/gin-rate-limit"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"time"
 )
 
 type Dependency struct {
@@ -25,10 +28,23 @@ func NewDependency() *Dependency {
 
 	weatherHandler := handlers.NewWeatherHandler(weatherClient)
 	prometheusHandler := handlers.NewPrometheusHandler()
+
+	store := ratelimit.RedisStore(&ratelimit.RedisOptions{
+		RedisClient: redis.NewClient(&redis.Options{
+			Addr: "redis:6379",
+		}),
+		Rate:  time.Second,
+		Limit: 1,
+	})
+	mw := ratelimit.RateLimiter(store, &ratelimit.Options{
+		ErrorHandler: rateLimitErrorHandler,
+		KeyFunc:      rateLimitKeyFunc,
+	})
+
 	e := gin.Default()
 	e.Use(prometheusHandler.RPSMiddleware)
 	e.Handle("GET", "/metrics", prometheusHandler.HandleMetrics)
-	e.Handle("GET", "/", weatherHandler.HandleForecast)
+	e.Handle("GET", "/", mw, weatherHandler.HandleForecast)
 
 	return &Dependency{
 		Config: &cfg,
@@ -37,4 +53,12 @@ func NewDependency() *Dependency {
 
 		GinEngine: e,
 	}
+}
+
+func rateLimitKeyFunc(c *gin.Context) string {
+	return c.ClientIP()
+}
+
+func rateLimitErrorHandler(c *gin.Context, info ratelimit.Info) {
+	c.String(429, "Too many requests. Try again in "+time.Until(info.ResetTime).String())
 }
